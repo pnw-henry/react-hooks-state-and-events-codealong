96c3daf569e399957e43235d50e6e5da9c3745ad
Henry Escobar <henrye@gmail.com>
2022-08-03 14:03:08 -0700

    commit tree b38d1c364c25e50e41969a71eeb34acbe9ff32f2

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+  const [status, setState] = useState(false);
+  const color = status ? "red" : "white";
+
+  function handleClick() {
+    setState((status) => !status);
+  }
+  return (
+    <button onClick={handleClick} style={{ background: color }}>
+      {status ? "ON" : "OFF"}
+    </button>
+  );
 }
 
 export default Toggle;
